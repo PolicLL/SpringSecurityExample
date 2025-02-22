a09d962289fd516e6a6733cf9fe19f4772a522cb
Luka <LPolic@its.jnj.com>
2025-02-22 11:45:02 +0100

    Some more playing with users and UserDetailsService.

--- SpringSecurityExample/src/main/java/com/example/SpringSecurityExample/config/SecurityConfig.java
+++ SpringSecurityExample/src/main/java/com/example/SpringSecurityExample/config/SecurityConfig.java
@@ -7,6 +7,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,21 +20,30 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityWebFilterChain(HttpSecurity httpSecurity) throws Exception {
-//    return httpSecurity.csrf(AbstractHttpConfigurer::disable)
-//        .authorizeHttpRequests(request -> request.anyRequest().authenticated())
-//        .formLogin(Customizer.withDefaults()) // include form login for browser
-//        .httpBasic(Customizer.withDefaults()) // include it so that Auth works for Postman
-//        .build();
+    return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(request -> request.anyRequest().authenticated())
+        .formLogin(Customizer.withDefaults()) // include form login for browser
+        .httpBasic(Customizer.withDefaults()) // include it so that Auth works for Postman
+        .build();
+  }
 
+  @Bean
+  public UserDetailsService userDetailsService() {
+    UserDetails userDetails1 = User
+        .withDefaultPasswordEncoder()
+        .username("Pera")
+        .password("Pera")
+        .roles("USER")
+        .build();
 
-    Customizer<CsrfConfigurer<HttpSecurity>> customCsrf = customizer -> {
+    UserDetails userDetails2 = User
+        .withDefaultPasswordEncoder()
+        .username("Ivan")
+        .password("Ivic")
+        .roles("ADMIN")
+        .build();
 
-    };
-
-    httpSecurity.csrf(customCsrf);
-
-    return httpSecurity.build();
-
+    return new InMemoryUserDetailsManager(userDetails1, userDetails2);
   }
 
 }
