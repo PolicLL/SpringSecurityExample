ba4384f4df9f52bf36a80c2b142d2666b68641f9
Luka <LPolic@its.jnj.com>
2025-02-24 13:24:57 +0100

    Finished generation of JWT.

--- SpringSecurityExample/src/main/java/com/example/SpringSecurityExample/service/JwtService.java
+++ SpringSecurityExample/src/main/java/com/example/SpringSecurityExample/service/JwtService.java
@@ -4,13 +4,28 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+  private final String secretKey;
+  public JwtService() {
+    try {
+      KeyGenerator keyGenerator = KeyGenerator.getInstance("HmacSHA256");
+      SecretKey sk = keyGenerator.generateKey();
+      secretKey = Base64.getEncoder().encodeToString(sk.getEncoded());
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   public String generateToken(String username) {
     Map<String, Object> claims = new HashMap<>();
@@ -20,13 +35,13 @@ public class JwtService {
         .add(claims)
         .subject(username)
         .issuedAt(new Date(System.currentTimeMillis()))
-        .expiration(new Date(System.currentTimeMillis() * 60 * 60 * 30))
+        .expiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 3))
         .and()
         .signWith(getKey())
         .compact();
   }
 
   private Key getKey() {
-    return Keys.hmacShaKeyFor(Decoders.BASE64.decode("5hfesf"));
+    return Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
   }
 }
